ce35639d303c5afb8021645098c1bf3425bb9e50
Faycel BELGACEM <faycel@l2paysagiste.fr>
2020-10-08 22:47:42 +0200

    [FIX] project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AuthenticationComponent } from './pages/public/authentication/authentication.component';
+import { PublicComponent } from './pages/public/public.component';
+import { RegisterComponent } from './pages/public/register/register.component';
+import { FooterComponent } from './_shared/layout/footer/footer.component';
+import { HeaderComponent } from './_shared/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    PublicComponent,
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
