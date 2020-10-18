c427048edc711cb9096bd5c6d0c6eef49d7d01c8
Faycel BELGACEM <faycel@l2paysagiste.fr>
2020-10-18 18:34:42 +0200

    [ADD] header

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { FooterComponent } from './../../_shared/layout/footer/footer.component';
+import { HeaderComponent } from './../../_shared/layout/header/header.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
   ]
 })
 export class DashboardModule { }
